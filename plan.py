556b299d15f0890c49fd8a0b00851109c64b9f9d
giackperetti <github.giack@gmail.com>
2024-05-19 11:40:19 +0200

    added GUI and support for word files

--- plan.py
+++ plan.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+from typing import Dict
+from docx import Document
 import random
+import tkinter as tk
 
 
 def process_data(file_name):
@@ -26,7 +29,7 @@ def is_valid_pair(lunch, dinner):
     return dinner in valid_pairs.get(lunch, [])
 
 
-def planMaker(breakfast_data, lunch_data, dinner_data):
+def generate_plan(breakfast_data: Dict, lunch_data: Dict, dinner_data: Dict):
     diet_plan = []
     for week in range(4):
         week_plan = []
@@ -81,6 +84,75 @@ def planMaker(breakfast_data, lunch_data, dinner_data):
     return diet_plan
 
 
+def save_plan_txt(breakfast_data: Dict, lunch_data: Dict, dinner_data: Dict):
+    diet_plan = generate_plan(breakfast_data, lunch_data, dinner_data)
+
+    with open("diet_plan.txt", "w") as output_file:
+        output_file.write("Piano Dietetico(4 settimane):\n\n")
+        for i, week in enumerate(diet_plan, 1):
+            output_file.write(f"Settimana {i}:\n")
+            for day, meal in enumerate(week, 1):
+                output_file.write(f"Giorno {day}:\n")
+                output_file.write(f"Colazione: \n{meal[0]}\n\n")
+                output_file.write(f"Spuntino Mattutino: \n{meal[1]}\n\n")
+                output_file.write(f"Pranzo: \n{meal[2]}\n\n")
+                output_file.write(f"Spuntino Pomeridiano: \n{meal[3]}\n\n")
+                output_file.write(f"Cena: \n{meal[4]}\n\n")
+            output_file.write("\n")
+
+    print("Diet plan saved to diet_plan.txt")
+
+
+def save_plan_docx(breakfast_data: Dict, lunch_data: Dict, dinner_data: Dict):
+    diet_plan = generate_plan(breakfast_data, lunch_data, dinner_data)
+
+    doc = Document()
+    doc.add_heading("Piano Dietetico (4 settimane)", level=1)
+
+    for i, week in enumerate(diet_plan, 1):
+        doc.add_heading(f"Settimana {i}", level=2)
+        for day, meal in enumerate(week, 1):
+            doc.add_heading(f"Giorno {day}", level=3)
+            doc.add_heading("Colazione:", level=4)
+            doc.add_paragraph(meal[0])
+            doc.add_heading("Spuntino Mattutino:", level=4)
+            doc.add_paragraph(meal[1])
+            doc.add_heading("Pranzo:", level=4)
+            doc.add_paragraph(meal[2])
+            doc.add_heading("Spuntino Pomeridiano:", level=4)
+            doc.add_paragraph(meal[3])
+            doc.add_heading("Cena:", level=4)
+            doc.add_paragraph(meal[4])
+
+    doc.save("diet_plan.docx")
+    print("Diet plan saved to diet_plan.docx")
+
+
+def show_gui(breakfast_data: Dict, lunch_data: Dict, dinner_data: Dict):
+    root = tk.Tk()
+    root.title("Generatore Piano Dietetico")
+    root.resizable(False, False)
+
+    title = tk.Label(root, text="Generatore Piano Dietetico")
+    title.pack(padx=20, pady=20)
+
+    button_txt = tk.Button(
+        root,
+        text="Salva il piano dietetico da 4 settimane come file di testo(.txt)",
+        command=lambda: save_plan_txt(breakfast_data, lunch_data, dinner_data),
+    )
+    button_txt.pack(padx=20, pady=20)
+
+    button_docx = tk.Button(
+        root,
+        text="Salva il piano dietetico da 4 settimane come file word(.docx)",
+        command=lambda: save_plan_docx(breakfast_data, lunch_data, dinner_data),
+    )
+    button_docx.pack(padx=10, pady=10)
+
+    root.mainloop()
+
+
 def main():
     breakfast = "breakfast.txt"
     lunch = "lunch.txt"
@@ -89,22 +161,7 @@ def main():
     lunch_data = process_data(lunch)
     dinner_data = process_data(dinner)
 
-    diet_plan = planMaker(breakfast_data, lunch_data, dinner_data)
-
-    with open("diet_plan.txt", "w") as output_file:
-        output_file.write("Diet plan:\n\n")
-        for i, week in enumerate(diet_plan, 1):
-            output_file.write(f"Week {i}:\n")
-            for day, meal in enumerate(week, 1):
-                output_file.write(f"Day {day}:\n")
-                output_file.write(f"Breakfast: \n{meal[0]}\n\n")
-                output_file.write(f"Spuntino Mattutino: \n{meal[1]}\n\n")
-                output_file.write(f"Lunch: \n{meal[2]}\n\n")
-                output_file.write(f"Spuntino Pomeridiano: \n{meal[3]}\n\n")
-                output_file.write(f"Dinner: \n{meal[4]}\n\n")
-            output_file.write("\n")
-
-    print("Diet plan saved to diet_plan.txt")
+    show_gui(breakfast_data, lunch_data, dinner_data)
 
 
 if __name__ == "__main__":
